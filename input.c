a6e5c273187f3bdcd2105e45027db0f3d4b6c8b1
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-04-02 15:36:02 +0530

    Compiler Design codes 2nd april Lexical analyser in C and Lex code

--- input.c
+++
@@ -1,4 +0,0 @@
-int a, b;
-sum  = a + b;
-print("sum is ", sum);
-return 0;
---
+++ lex_input.c
@@ -0,0 +1,9 @@
+// This is a Comment
+/*
+This is multiline comment
+*/
+int a = 3;
+int b = 5;
+sum = a + b;
+printf("Sum is : %d", sum);
+return 0;
